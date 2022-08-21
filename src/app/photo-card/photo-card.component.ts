3b78e1f2b394172b3212566b82c512b25c704993
afolabiopakunle <opakunleafolabi@gmail.com>
2022-08-21 23:00:54 +0100

    background filter implemented

--- src/app/photo-card/photo-card.component.ts
+++ src/app/photo-card/photo-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
 
 @Component({
   selector: 'app-photo-card',
@@ -7,10 +7,31 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class PhotoCardComponent implements OnInit {
 
+  @ViewChild('modal_1') modal_1!: TemplateRef<any>;
+  @ViewChild('vc', {read: ViewContainerRef}) vc!: ViewContainerRef;
+
   constructor() { }
   @Input() photo: any;
 
   ngOnInit(): void {
   }
 
+
+  backdrop: any
+  showModal(photo: any){
+    let view = this.modal_1.createEmbeddedView(null);
+    this.vc.insert(view);
+    this.modal_1.elementRef.nativeElement.previousElementSibling.classList.remove('fade');
+    this.modal_1.elementRef.nativeElement.previousElementSibling.classList.add('modal-open');
+    this.modal_1.elementRef.nativeElement.previousElementSibling.style.display = 'block';
+    this.backdrop = document.createElement('DIV')
+    this.backdrop.className = 'modal-backdrop';
+    document.body.appendChild(this.backdrop);
+    console.log(photo)
+  }
+
+  closeModal() {
+    this.vc.clear()
+    document.body.removeChild(this.backdrop)
+  }
 }
